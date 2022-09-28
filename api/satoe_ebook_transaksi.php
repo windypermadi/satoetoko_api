ac20cb7a3d61574eff639255cf870372fa99a35f
windypermadi29 <whindy21official@gmail.com>
2022-09-28 15:34:42 +0700

    28/09/2022

--- api/satoe_ebook_transaksi.php
+++ api/satoe_ebook_transaksi.php
@@ -20,7 +20,7 @@ switch ($tag) {
         a.diskon_persen, a.harga_sewa, a.diskon_sewa_rupiah, a.diskon_sewa_persen, b.sinopsis,
         b.penerbit, b.tahun_terbit, b.tahun_terbit, b.edisi, b.isbn, b.status_ebook, b.lama_sewa FROM master_item a 
         JOIN master_ebook_detail b ON a.id_master = b.id_master
-        JOIN kategori_sub c ON a.id_sub_kategori = c.id_sub WHERE a.status_master_detail = '1' AND a.id_master = '$id_master'"));
+        LEFT JOIN kategori_sub c ON a.id_sub_kategori = c.id_sub WHERE a.status_master_detail = '1' AND a.id_master = '$id_master'"));
 
         if ($status == '1') {
             $diskon_persen = $data->diskon_persen;
