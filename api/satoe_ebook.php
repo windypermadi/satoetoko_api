02ab43d9b52361a628de8ca5d790a9c8fc9ed8df
windypermadi29 <whindy21official@gmail.com>
2022-10-13 14:22:54 +0700

    13/10/22

--- api/satoe_ebook.php
+++ api/satoe_ebook.php
@@ -54,7 +54,7 @@ switch ($tag) {
 
 		$data1['id_master']    = $data->id_master;
 		$data1['judul_master'] = $data->judul_master;
-		$data1['image_master'] = $urlimg.$data->image_master;
+		$data1['image_master'] = $urlimg . $data->image_master;
 		$data1['status_ebook'] = $data->status_ebook;
 		$data1['rating_ebook'] = 0;
 		$data1['nama_kategori'] = $data->nama_kategori;
@@ -114,7 +114,7 @@ switch ($tag) {
 			} else {
 				$status_ebook = '1';
 			}
-	
+
 			if ($value['diskon_persen'] != 0) {
 				(float)$harga_potongan = (float)$value['harga_master'] * ((float)$value['diskon_persen'] / 100);
 				(float)$harga_disc = $value['harga_master'] - $harga_potongan;
@@ -127,7 +127,7 @@ switch ($tag) {
 				$jumlah_diskon = "0";
 				$harga_disc = (int)$value['harga_master'];
 			}
-	
+
 			if ($value['diskon_sewa_persen'] != 0) {
 				(float)$harga_potongan_sewa = (float)$value['harga_sewa'] * ((float)$value['diskon_sewa_persen'] / 100);
 				(float)$harga_disc_sewa = $value['harga_sewa'] - $harga_potongan;
@@ -144,7 +144,100 @@ switch ($tag) {
 			array_push($datalist, array(
 				'id_master' => $value['id_master'],
 				'judul_master' => $value['judul_master'],
-				'image_master' => $urlimg.$value['image_master'],
+				'image_master' => $urlimg . $value['image_master'],
+				'status_ebook' => $value['status_ebook'],
+				'rating_ebook' => 0,
+				'nama_kategori' => $value['nama_kategori'],
+				'sinopsis' => $value['sinopsis'],
+				'lama_sewa' => $value['lama_sewa'],
+				'harga_beli' => (int)$value['harga_master'],
+				'diskon_beli' => (int)$jumlah_diskon,
+				'harga_diskon_beli' => (int)$harga_disc,
+				'harga_sewa' => (int)$value['harga_sewa'],
+				'diskon_sewa' => (int)$jumlah_diskon_sewa,
+				'harga_diskon_sewa' => (int)$harga_disc_sewa,
+			));
+		}
+
+		if (isset($datalist[0])) {
+			$response->code = 200;
+			$response->message = 'result';
+			$response->data = $datalist;
+			$response->json();
+			die();
+		} else {
+			$response->code = 200;
+			$response->message = 'Tidak ada data yang ditampilkan!\nKlik `Mengerti` untuk menutup pesan ini';
+			$response->data = [];
+			$response->json();
+			die();
+		}
+		break;
+	case "kategori":
+		$limit = $_GET['limit'];
+		$offset = $_GET['offset'];
+		$id_kategori = $_GET['id_kategori'] ?? '';
+		$q = $_GET['q'] ?? '';
+		if (empty($q)) {
+			$datalist = array();
+			$data = $conn->query("SELECT a.id_master, a.judul_master, a.image_master, c.nama_kategori, a.harga_master, a.diskon_rupiah, 
+					a.diskon_persen, a.harga_sewa, a.diskon_sewa_rupiah, a.diskon_sewa_persen, b.sinopsis,
+					b.penerbit, b.tahun_terbit, b.tahun_terbit, b.edisi, b.isbn, b.status_ebook, b.lama_sewa FROM master_item a 
+					JOIN master_ebook_detail b ON a.id_master = b.id_master
+					JOIN kategori_sub c ON a.id_sub_kategori = c.id_sub WHERE a.status_master_detail = '1' AND a.id_sub_kategori = '$id_kategori' ORDER BY a.judul_master ASC LIMIT $offset, $limit");
+		} else {
+			$datalist = array();
+			$data = $conn->query("SELECT a.id_master, a.judul_master, a.image_master, c.nama_kategori, a.harga_master, a.diskon_rupiah, 
+					a.diskon_persen, a.harga_sewa, a.diskon_sewa_rupiah, a.diskon_sewa_persen, b.sinopsis,
+					b.penerbit, b.tahun_terbit, b.tahun_terbit, b.edisi, b.isbn, b.status_ebook, b.lama_sewa FROM master_item a 
+					JOIN master_ebook_detail b ON a.id_master = b.id_master
+					JOIN kategori_sub c ON a.id_sub_kategori = c.id_sub WHERE a.status_master_detail = '1' AND a.id_sub_kategori = '$id_kategori' AND a.judul_master LIKE '%$q%' ORDER BY a.judul_master ASC LIMIT $offset, $limit");
+		}
+
+		foreach ($data as $key => $value) {
+			if ($value['status_ebook'] == '1') {
+				//beli
+				$status_ebook = '1';
+			} else if ($value['status_ebook'] == '2') {
+				//sewa
+				$status_ebook = '2';
+			} else if ($value['status_ebook'] == '3') {
+				//beli dan sewa
+				$status_ebook = '3';
+			} else {
+				$status_ebook = '1';
+			}
+
+			if ($value['diskon_persen'] != 0) {
+				(float)$harga_potongan = (float)$value['harga_master'] * ((float)$value['diskon_persen'] / 100);
+				(float)$harga_disc = $value['harga_master'] - $harga_potongan;
+				$jumlah_diskon = $value['diskon_persen'];
+			} else if ($value['diskon_rupiah'] != 0) {
+				(float)$harga_disc = $value['harga_master'] - $value['diskon_rupiah'];
+				$jumlah_diskon = $value['diskon_rupiah'];
+			} else {
+				$harga_potongan = 0;
+				$jumlah_diskon = "0";
+				$harga_disc = (int)$value['harga_master'];
+			}
+
+			if ($value['diskon_sewa_persen'] != 0) {
+				(float)$harga_potongan_sewa = (float)$value['harga_sewa'] * ((float)$value['diskon_sewa_persen'] / 100);
+				(float)$harga_disc_sewa = $value['harga_sewa'] - $harga_potongan;
+				$jumlah_diskon_sewa = $value['diskon_sewa_persen'];
+			} else if ($value['diskon_sewa_rupiah'] != 0) {
+				(float)$harga_disc_sewa = $value['harga_sewa'] - $value['diskon_sewa_rupiah'];
+				$jumlah_diskon_sewa = $value['diskon_sewa_rupiah'];
+			} else {
+				$harga_potongan_sewa = 0;
+				$jumlah_diskon_sewa = "0";
+				$harga_disc_sewa = (int)$value['harga_sewa'];
+			}
+
+			array_push($datalist, array(
+				'id_master' => $value['id_master'],
+				'judul_master' => $value['judul_master'],
+				'image_master' => $urlimg . $value['image_master'],
 				'status_ebook' => $value['status_ebook'],
 				'rating_ebook' => 0,
 				'nama_kategori' => $value['nama_kategori'],
