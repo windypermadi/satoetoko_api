6729854bc6c148f5e032bd43247f07ed1b4b26cd
windypermadi29 <whindy21official@gmail.com>
2022-10-18 16:45:53 +0700

    midtrans

--- api/midtrans/notif_new.php
+++ api/midtrans/notif_new.php
@@ -33,22 +33,23 @@ if (empty($data_kepala)) {
 if ($data['transaction_status'] == "settlement" or $data['transaction_status'] == "capture" or $data['transaction_status'] == "accept") {
 
     $conn->begin_transaction();
-    // $sql    = mysqli_query($conn, "SELECT a.id_transaksi_detail, a.status_pembelian, d.lama_sewa FROM ebook_transaksi_detail a 
-    // JOIN ebook_transaksi b ON a.id_transaksi = b.id_transaksi
-    // JOIN master_item c ON a.id_master = c.id_master
-    // JOIN master_ebook_detail d ON c.id_master = d.id_master
-    // WHERE b.invoice = '$order_id'");
-    // while ($row = mysqli_fetch_array($sql)) {
-    //     if ($row['status_pembelian'] == '1') {
-    //         $lama = 3650;
-    //     } else if ($row['status_pembelian'] == '2') {
-    //         $lama = $row['lama_sewa'];
-    //     }
-    //     $stmt[]    = mysqli_query($conn, "UPDATE ebook_transaksi_detail SET tgl_expired = DATE_ADD(NOW(), 
-    //     INTERVAL '$lama' DAY) WHERE id_transaksi_detail = '$row[id_transaksi_detail]'");
-    // }
-
+    $sql    = mysqli_query($conn, "SELECT a.id_transaksi_detail, a.status_pembelian, d.lama_sewa FROM ebook_transaksi_detail a 
+    JOIN ebook_transaksi b ON a.id_transaksi = b.id_transaksi
+    JOIN master_item c ON a.id_master = c.id_master
+    JOIN master_ebook_detail d ON c.id_master = d.id_master
+    WHERE b.invoice = '$order_id'");
+    while ($row = mysqli_fetch_array($sql)) {
+        if ($row['status_pembelian'] == '1') {
+            $lama = 3650;
+        } else if ($row['status_pembelian'] == '2') {
+            $lama = $row['lama_sewa'];
+        }
+        $stmt[]    = mysqli_query($conn, "UPDATE ebook_transaksi_detail SET tgl_expired = DATE_ADD(NOW(), 
+        INTERVAL '$lama' DAY) WHERE id_transaksi_detail = '$row[id_transaksi_detail]'");
+    }
     $stmt[] = $conn->query("UPDATE ebook_transaksi SET status_transaksi= '7', tgl_dibayar = NOW(), payment_type='" . $data['payment_type'] . "' WHERE invoice = '$order_id'");
+
+    // $stmt[] = $conn->query("UPDATE ebook_transaksi SET status_transaksi= '7', tgl_dibayar = NOW(), payment_type='" . $data['payment_type'] . "' WHERE invoice = '$order_id'");
     if (in_array(false, $stmt) or in_array(0, $stmt)) {
         $conn->rollback();
         $response->code = 400;
