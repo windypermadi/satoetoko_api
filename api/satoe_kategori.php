e40e370469311bb0f7497200782d7315eed1db53
windypermadi29 <whindy21official@gmail.com>
2022-10-20 15:52:21 +0700

    ebook

--- api/satoe_kategori.php
+++ api/satoe_kategori.php
@@ -6,7 +6,7 @@ $response = new Response();
 $query = mysqli_query($conn, "SELECT a.id_sub, a.kode_kategori, a.nama_kategori, a.icon FROM kategori_sub a 
 JOIN kategori b ON a.parent_kategori = b.id_kategori
 JOIN master_item c ON c.id_sub_kategori = a.id_sub
-WHERE b.jenis_kategori = '1' AND c.status_master_detail = '1' ORDER BY a.nama_kategori ASC");
+WHERE b.jenis_kategori = '1' AND c.status_master_detail = '1' GROUP BY a.id_sub ORDER BY a.nama_kategori ASC");
 
 $result = array();
 while($row = mysqli_fetch_array($query)){
