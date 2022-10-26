5e24da00f8dff7d78b6a3bd8ee3bcd0f1d9a635c
windypermadi29 <whindy21official@gmail.com>
2022-10-26 12:40:10 +0700

    26/10/2022

--- config/koneksi.php
+++ config/koneksi.php
@@ -51,8 +51,8 @@ $getkategoriicon = "https://devsertidemi.andipublisher.com/image/kategori/";
 
 //satoetoko server production
 $urlpdf = "https://satoetoko.com/dashboard/files/upload/ebook/";
+$urlpdf_obral = "http://103.137.254.78/andibook/pdfs/";
 $urlimg = "https://satoetoko.com/assets/images/products/ebook/";
-// $urlpdf = "http://103.137.254.78/andibook/pdfs/";
 $getprofile = "https://satoetoko.com/assets/images/pict-profile/";
 $urlbanner = "https://satoetoko.com/assets/images/banner/";
 $geticonkategori = "https://satoetoko.com/assets/images/icon_kategori/";
