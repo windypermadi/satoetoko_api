af85638a53d4c9881986137a454822fa009e09fe
windypermadi29 <whindy21official@gmail.com>
2022-10-26 14:49:34 +0700

    cek transaksi buku sudah ada atau belum

--- config/koneksi.php
+++ config/koneksi.php
@@ -50,8 +50,8 @@ $getkategoriicon = "https://devsertidemi.andipublisher.com/image/kategori/";
 // $geticonpayment = "http://dev.satoetoko.com/assets/images/products/ebook/";
 
 //satoetoko server production
-$urlpdf = "https://satoetoko.com/dashboard/files/upload/ebook/";
-// $urlpdf = "http://103.137.254.78/andibook/pdfs/";
+// $urlpdf = "https://satoetoko.com/dashboard/files/upload/ebook/";
+$urlpdf = "http://103.137.254.78/andibook/pdfs/";
 $urlimg = "https://satoetoko.com/assets/images/products/ebook/";
 $getprofile = "https://satoetoko.com/assets/images/pict-profile/";
 $urlbanner = "https://satoetoko.com/assets/images/banner/";
