01776e465bca5d588832f6b08aa1ac1ce7c2302b
windypermadi <whindy21official@gmail.com>
2022-11-10 16:36:11 +0700

    error variant sama di cart

--- api/st_addcart.php
+++ api/st_addcart.php
@@ -11,23 +11,46 @@ $jumlah           = $_POST['jumlah'];
 
 if (isset($id_login) && isset($id_cabang) && isset($id_master) && isset($jumlah)) {
 
-    $q = "SELECT id,qty FROM user_keranjang WHERE id_user = 
-'$id_login' AND id_barang = '$id_master' AND id_gudang = '$id_cabang'";
-    $cekitemdata = $conn->query($q);
-
-    if ($cekitemdata->num_rows > 0) {
+    if (is_null($id_variant)) {
+        $q = "SELECT id,qty FROM user_keranjang WHERE id_user = 
+                '$id_login' AND id_barang = '$id_master' AND id_gudang = '$id_cabang'";
+        $cekitemdata = $conn->query($q);
         $data = $cekitemdata->fetch_object();
-        $qty = $data->qty;
-        $qty = $qty + $jumlah;
-        $query = mysqli_query($conn, "UPDATE user_keranjang SET qty = '$qty' WHERE id = '$data->id'");
-    } else {
-        $qty = $jumlah;
-        $query = mysqli_query($conn, "INSERT INTO user_keranjang SET id = UUID(),
+
+        if ($cekitemdata->num_rows > 0) {
+            $data = $cekvariant->fetch_object();
+            $qty = $data->qty;
+            $qty = $qty + $jumlah;
+            $query = mysqli_query($conn, "UPDATE user_keranjang SET qty = '$qty' WHERE id = '$data->id'");
+        } else {
+            $qty = $jumlah;
+            $query = mysqli_query($conn, "INSERT INTO user_keranjang SET id = UUID(),
                         id_user='$id_login',
                         id_barang='$id_master',
                         id_variant='$id_variant',
                         id_gudang='$id_cabang',
                         qty='$qty'");
+        }
+    } else {
+        $q = "SELECT id,qty FROM user_keranjang WHERE id_user = 
+                '$id_login' AND id_barang = '$id_master' AND id_gudang = '$id_cabang' AND id_variant = '$id_variant'";
+        $cekitemdata = $conn->query($q);
+        $data = $cekitemdata->fetch_object();
+
+        if ($cekitemdata->num_rows > 0) {
+            $data = $cekvariant->fetch_object();
+            $qty = $data->qty;
+            $qty = $qty + $jumlah;
+            $query = mysqli_query($conn, "UPDATE user_keranjang SET qty = '$qty' WHERE id = '$data->id'");
+        } else {
+            $qty = $jumlah;
+            $query = mysqli_query($conn, "INSERT INTO user_keranjang SET id = UUID(),
+                        id_user='$id_login',
+                        id_barang='$id_master',
+                        id_variant='$id_variant',
+                        id_gudang='$id_cabang',
+                        qty='$qty'");
+        }
     }
 
     if ($query) {
--- api/st_getcart.php
+++ api/st_getcart.php
@@ -4,34 +4,123 @@ include "response.php";
 $response = new Response();
 
 $id_login         = $_GET['id_login'];
+$tag              = $_GET['tag'];
 
 if (isset($id_login)) {
 
-    $data = $conn->query("SELECT * FROM user_keranjang WHERE id_user = '$id_login'");
+    switch ($tag) {
+        case 'semua':
+            $data = $conn->query("SELECT * FROM user_keranjang a
+            JOIN master_item b ON a.id_barang = b.id_master
+            LEFT JOIN variant c ON a.id_variant = c.id_variant
+            WHERE a.id_user = '$id_login';");
+            $datalist = array();
 
-    if ($data) {
-        $datalist = array();
+            foreach ($data as $key => $value) {
 
-        $data = $cekitemdata->fetch_object();
-        $qty = $data->qty;
-        $qty = $qty + $jumlah;
-        $query = mysqli_query($conn, "UPDATE user_keranjang SET qty = '$qty' WHERE id = '$data->id'");
-    } else {
-        $qty = $jumlah;
-        $query = mysqli_query($conn, "INSERT INTO user_keranjang SET id = UUID(),
-                        id_user='$id_login',
-                        id_barang='$id_master',
-                        id_variant='$id_variant',
-                        id_gudang='$id_cabang',
-                        qty='$qty'");
-    }
+                if ($value['diskon_persen'] != 0) {
+                    $status_diskon = 'Y';
+                    (float)$harga_disc = $value['harga_master'] - $value['diskon_rupiah'];
+                } else {
+                    $status_diskon = 'N';
+                    (float)$harga_disc = $value['harga_master'];
+                }
 
-    if ($query) {
-        $response->data = null;
-        $response->sukses(200);
-    } else {
-        $response->data = null;
-        $response->error(400);
+                $harga_produk = "Rp" . number_format($value['harga_master'], 0, ',', '.');
+                $harga_tampil = "Rp" . number_format($harga_disc, 0, ',', '.');
+
+                if (!is_null($value['id_variant'])) {
+                    $id_variant = $_GET['id_variant'];
+                    $data = $conn->query("SELECT * FROM user_keranjang a
+                    JOIN master_item b ON a.id_barang = b.id_master
+                    LEFT JOIN variant c ON a.id_variant = c.id_variant
+                    WHERE a.id_user = '$id_login' AND a.id_variant = '$id_variant';");
+                    $datalist = array();
+                } else {
+                    $data = $conn->query("SELECT * FROM user_keranjang a
+                    JOIN master_item b ON a.id_barang = b.id_master
+                    LEFT JOIN variant c ON a.id_variant = c.id_variant
+                    WHERE a.id_user = '$id_login';");
+                    $datalist = array();
+                }
+
+                array_push($datalist, array(
+                    'id' => $value['id'],
+                    'image_master' => $urlimg . $value['image_master'],
+                    'judul' => $value['judul_master'],
+                    'varian' => $value['keterangan_varian'],
+                    'harga_produk' => $harga_produk,
+                    'harga_tampil' => $harga_tampil,
+                    'qty' => $value['qty'],
+                ));
+            }
+
+            if ($datalist[0]) {
+                $response->data = $datalist;
+                $response->sukses(200);
+            } else {
+                $response->data = null;
+                $response->error(400);
+            }
+            break;
+        case 'diskon':
+            $data = $conn->query("SELECT * FROM user_keranjang a
+JOIN master_item b ON a.id_barang = b.id_master
+LEFT JOIN variant c ON a.id_variant = c.id_variant
+WHERE a.id_user = '$id_login' AND b.diskon_persen != 0");
+            $datalist = array();
+
+            foreach ($data as $key => $value) {
+
+                if ($value['diskon_persen'] != 0) {
+                    $status_diskon = 'Y';
+                    (float)$harga_disc = $value['harga_master'] - $value['diskon_rupiah'];
+                } else {
+                    $status_diskon = 'N';
+                    (float)$harga_disc = $value['harga_master'];
+                }
+
+                $harga_produk = "Rp" . number_format($value['harga_master'], 0, ',', '.');
+                $harga_tampil = "Rp" . number_format($harga_disc, 0, ',', '.');
+
+                if (!is_null($value['id_variant'])) {
+                    $id_variant = $_GET['id_variant'];
+                    $data = $conn->query("SELECT * FROM user_keranjang a
+                    JOIN master_item b ON a.id_barang = b.id_master
+                    LEFT JOIN variant c ON a.id_variant = c.id_variant
+                    WHERE a.id_user = '$id_login' AND a.id_variant = '$id_variant';");
+                    $datalist = array();
+                } else {
+                    $data = $conn->query("SELECT * FROM user_keranjang a
+                    JOIN master_item b ON a.id_barang = b.id_master
+                    LEFT JOIN variant c ON a.id_variant = c.id_variant
+                    WHERE a.id_user = '$id_login';");
+                    $datalist = array();
+                }
+
+                array_push($datalist, array(
+                    'id' => $value['id'],
+                    'image_master' => $urlimg . $value['image_master'],
+                    'judul' => $value['judul_master'],
+                    'varian' => $value['keterangan_varian'],
+                    'harga_produk' => $harga_produk,
+                    'harga_tampil' => $harga_tampil,
+                    'qty' => $value['qty'],
+                ));
+            }
+
+            if ($datalist[0]) {
+                $response->data = $datalist;
+                $response->sukses(200);
+            } else {
+                $response->data = null;
+                $response->error(400);
+            }
+
+            break;
+        case 'repeat':
+
+            break;
     }
 } else {
     $response->data = null;
