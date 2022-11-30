e01c97a712529c10abee4c187f373c6a5654883d
windypermadi <whindy21official@gmail.com>
2022-11-30 16:43:49 +0700

    add payment

--- api/st_addcheckout.php
+++ api/st_addcheckout.php
@@ -46,6 +46,7 @@ foreach ($dataproduk as $i => $key) {
  LEFT JOIN master_buku_detail d ON b.id_master = d.id_master
  LEFT JOIN master_fisik_detail e ON b.id_master = e.id_master
  LEFT JOIN supplier f ON b.id_supplier = f.id_supplier
+ LEFT JOIN stok g ON b.id_master = g.id_barang
  WHERE a.id = '$key[id_cart]'")->fetch_object();
 }
 foreach ($getproduk as $u) {
@@ -69,6 +70,7 @@ foreach ($getproduk as $u) {
             'harga_tampil' => $u->diskon_rupiah_varian != 0 ? ($diskon_format) : $harga_varian
         ];
 
+        //! INSERT TRANSAKSI DETAIL
         $harga_diskon = $u->harga_varian - $u->diskon_rupiah_varian;
         $feetoko = $harga_diskon - ($harga_diskon * ($u->fee_admin / 100));
         $sbtotal = round($harga_diskon * $u->qty);
@@ -85,6 +87,26 @@ foreach ($getproduk as $u) {
         berat = '$berat',
         fee_toko = '$feetoko',  
         sub_total = '$sbtotal'");
+
+        //! UPDATE STOK PRODUCT
+        $query[] = $conn->query("SELECT jumlah FROM stok WHERE id_varian = '$u->id_variant'")->fetch_assoc();
+        $hasiljumlah = $query['jumlah'] - $u->qty;
+
+        $query[] = $conn->query("UPDATE stok SET jumlah = '$hasiljumlah' WHERE id_varian = '$u->id_variant'");
+
+        //! UPDATE STOK HISTORY PRODUCT
+        // $query[] = $conn->query("INSERT INTO stok_history SET 
+        // id_history = UUID_SHORT(),
+        // tanggal_input = '$u->id_master',
+        // master_item = '$u->id_supplier',
+        // varian_item = '$u->harga_master',
+        // id_warehouse = '$u->diskon_rupiah',
+        // keterangan = '$harga_diskon',
+        // masuk = '$u->qty',
+        // keluar = '$berat',
+        // stok_awal = '$feetoko',  
+        // stok_sekarang = '$sbtotal'");
+        // $query[] = $conn->query("UPDATE stok SET jumlah = '$hasiljumlah' WHERE id_varian = '$u->id_variant'");
     } else {
 
         $diskon = ($u->harga_master) - ($u->diskon_rupiah);
@@ -117,6 +139,12 @@ foreach ($getproduk as $u) {
         berat = '$berat',
         fee_toko = '$feetoko',  
         sub_total = '$sbtotal'");
+
+        //! UPDATE STOK PRODUCT
+        $query[] = $conn->query("SELECT jumlah FROM stok WHERE id_barang = '$u->id_master'")->fetch_assoc();
+        $hasiljumlah = $query['jumlah'] - $u->qty;
+
+        $query[] = $conn->query("UPDATE stok SET jumlah = '$hasiljumlah' WHERE id_barang = '$u->id_master'");
     }
 }
 
@@ -149,8 +177,92 @@ if (in_array(false, $query)) {
     $response->data = null;
     $response->error(400);
 } else {
-    $conn->commit();
-    $response->data = 'done';
-    $response->sukses(200);
-    die();
+
+    $querydata = mysqli_query($conn, "SELECT * FROM transaksi a 
+            JOIN data_user b ON a.id_user = b.id_login
+            WHERE a.id_transaksi = '$transaction->id'")->fetch_assoc();
+    $invoice = $querydata['invoice'];
+    $nama_user = $querydata['nama_user'];
+    $payer_email = $querydata['email'];
+    $no_telp = $querydata['notelp'];
+
+    if ($dataraw2['id_payment'] == '0') {
+        $mtrans['transaction_details']['order_id'] = $invoice;
+        $mtrans['transaction_details']['gross_amount'] = $dataraw->jumlahbayar;
+        $mtrans['credit_card']['secure'] = true;
+        $mtrans['customer_details']['first_name'] = $nama_user;
+        $mtrans['customer_details']['last_name'] = '';
+        $mtrans['customer_details']['email'] = $payer_email;
+        $mtrans['customer_details']['phone'] = $no_telp;
+        $mtrans_json = json_encode($mtrans);
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => MTRANS_URL,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $mtrans_json,
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Basic ' . base64_encode(MTRANS_SERVER_KEY),
+                'Content-Type: application/json'
+            ),
+        ));
+
+        $response_curl = curl_exec($curl);
+        curl_close($curl);
+
+        $responses = json_decode($response_curl, true);
+
+        $payment_url = $responses['redirect_url'];
+        $payment_token = $responses['token'];
+        $res['token'] = $payment_token;
+        $res['url_payment'] = $payment_url;
+
+        $query = mysqli_query($conn, "UPDATE transaksi SET midtrans_token = '$payment_token', midtrans_redirect_url = '$payment_url' WHERE id_transaksi= '$transaction->id'");
+
+        if (!isset($responses['token'])) {
+            respon_json_status_500();
+            $respon['pesan'] = "Sorry, we encountered internal server error. We will fix this soon.";
+            die(json_encode($respon));
+        }
+
+        $conn->commit();
+        $response->data = $res;
+        $response->sukses(200);
+        die();
+    } else {
+        // $total_format = "Rp" . number_format($jumlahbayar, 0, ',', '.');
+
+        // $result['batas_pembayaran'] = $exp_date;
+        $result['id_transaksi'] = $idtransaksi;
+        $result['invoice'] = $invoice;
+        // $result['id_payment'] = $id_payment;
+        // $result['icon_payment'] = $geticonpayment . $icon_payment;
+        // $result['metode_pembayaran'] = $metode_pembayaran;
+        // $result['nomor_payment'] = $nomor_payment;
+        // $result['penerima_payment'] = $penerima_payment;
+        // $result['total_harga'] = (int)$jumlahbayar;
+        // $result['nomor_konfirmasi'] = GETWA;
+        // $result['text_konfirmasi'] = "Halo Bapak/Ibu, Silahkan melakukan pembayaran manual dengan 
+        //     mengirimkan bukti transaksi.\n\nBerikut informasi tagihan anda : 
+        //         \nNomor Invoice : *$invoice*
+        //         \nJumlah     : *$total_format*
+        //         \nBank Transfer : *$metode_pembayaran*
+        //         \nNo Rekening : *$nomor_payment*
+        //         \nAtas Nama : *$penerima_payment*
+        //         \n\nJika ada pertanyaan lebih lanjut, anda dapat membalas langsung pesan ini.
+        //         \n\nTerimakasih\nHormat Kami, 
+        //         \n\nTim SatoeToko";
+
+        $conn->commit();
+        $response->data = "Pembayaran Harus Midtrans Otomatis";
+        $response->error(400);
+        die();
+    }
 }
