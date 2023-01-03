6f197b7baf16d2da9abd7468352f74b9e59144e7
windypermadi <whindy21official@gmail.com>
2023-01-03 09:09:51 +0700

    update

--- api/st_homeitem_unggulan.php
+++ api/st_homeitem_unggulan.php
@@ -8,7 +8,7 @@ $offset = $_GET['offset'];
 
 $result2 = array();
 $data = $conn->query("SELECT a.id_master, a.image_master, a.judul_master, a.harga_master, a.diskon_rupiah, a.diskon_persen,
-a.total_dibeli, a.total_disukai, SUM(b.jumlah) as jumlah, a.id_sub_kategori, c.nama_kategori, a.status_master_detail
+a.total_dibeli, a.total_disukai, SUM(b.jumlah) as jumlah, a.id_sub_kategori, c.nama_kategori, a.status_master_detail, a.status_varian
 FROM master_item a JOIN stok b ON a.id_master = b.id_barang
 JOIN kategori_sub c ON a.id_sub_kategori = c.id_sub
 LEFT JOIN master_buku_detail d ON a.id_master = d.id_master
